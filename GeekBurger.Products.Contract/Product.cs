b8fc2fd9aed66a0e03f877f806bb1572912e5807
Maxwell Freitas <maxfrts@gmail.com>
2020-08-24 00:43:35 -0300

    Adicionar arquivos de projeto.

---
+++ GeekBurger.Products.Contract/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GeekBurguer.Products.Contract
+{
+    public class Product
+    {
+        public string StoreName { get; set; }
+        public Guid ProductId { get; set; }
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public List<Item> Items { get; set; }
+        public decimal Price { get; set; }
+    }
+
+    public class Item
+    {
+        public Guid ItemId { get; set; }
+        public string Name { get; set; }
+    }
+
+}
---
+++ GeekBurger.Products.Contract/ProductChanged.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GeekBurguer.Products.Contract
+{
+    public class ProductChanged
+    {
+        public ProductState State { get; set; }
+        public Product Product { get; set; }
+    }
+
+    public enum ProductState
+    {
+        Deleted = 2,
+        Modified = 3,
+        Added = 4
+    }
+
+}
